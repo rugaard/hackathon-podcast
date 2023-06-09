611d9865a8f6bd2586749ed47a277db83fd72930
Morten Rugaard <morten@rugaard.me>
2023-06-09 10:37:18 +0200

    CSS tweaks to responsive desktop/tablet.

--- src/views/Podcasts.vue
+++ src/views/Podcasts.vue
@@ -3,9 +3,9 @@
     <header class="pb-10 border-b-2">
       <h2 class="text-20 md:text-28 font-semibold text-center">Podcasts</h2>
     </header>
-    <div class="grid grid-cols-1 md:grid-cols-3 md:gap-25 md:mt-25 border-b-2 divide-y-2 md:divide-y-0">
+    <div class="grid grid-cols-1 md:grid-cols-3 md:gap-25 md:mt-25 border-b-2 md:border-b-0 divide-y-2 md:divide-y-0">
       <template v-for="show in shows">
-        <RouterLink :to="{ name: 'podcast-show', params: { showId: show.id }}" class="px-20 odd:bg-gray-50">
+        <RouterLink :to="{ name: 'podcast-show', params: { showId: show.id }}" class="px-20 odd:bg-gray-50 md:odd:bg-transparent">
           <article class="col-span-1 py-20 md:py-0">
             <img :src="show.image.original" class="w-full rounded-xl" :alt="show.title" />
             <header class="mt-10">
